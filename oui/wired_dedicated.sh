8b18c6073b921b54a82c1268328d681b53e5fe73
i-ghost <i-ghost@wiki.tf>
2013-01-22 14:55:14 +0000

    forgot the router

--- oui/wired_dedicated.sh
+++ oui/wired_dedicated.sh
@@ -2,6 +2,7 @@ oui_wired_dedicated=(
 	64:70:02 # TP-Link Technologies CO., LTD.
 	b0:48:7a # Tp-link Technologies Co., Ltd. (SG3210 L2 managed switch)
 	00:26:f2 # Netgear (ReadyNAS Duo SPARC)
+	00:18:4D # Netgear Inc. (DG834GT ADSL Router)
 	b8:27:eb # Raspberry Pi Foundation (Rpi Model B 512)
 	00:50:f2 # Microsoft Corp. (Original Xbox 1st gen)
 	7c:ed:8d # Microsoft (Xbox 360 S 250GB Matte)
