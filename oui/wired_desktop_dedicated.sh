bb9d585714db89e99ea623d6949ba2a2b6486de9
Etienne Perot <etienne@perot.me>
2013-02-07 18:49:45 -0500

    Some more OUIs

--- oui/wired_desktop_dedicated.sh
+++ oui/wired_desktop_dedicated.sh
@@ -1,3 +1,4 @@
 oui_wired_desktop_dedicated=(
-	
+	00:14:d1 # TRENDnet (RTL81695C chipset, PCI chip, rev. B1)
+	00:50:ba # D-LINK (DL10038C, DFE-538TX rev. D2)
 )
--- oui/wireless_usb.sh
+++ oui/wireless_usb.sh
@@ -1,4 +1,6 @@
 oui_wireless_usb=(
+	00:0e:4c # Bermai Inc. (RTl8192cu 802.11n USB Adapter)
+	00:0f:13 # Nisca corporation (RTl8192cu low-power miniature 802.11n USB Adapter)
 	00:21:79 # Iogear, Inc. (IOGear GWU625 USB Adapter)
 	6c:fd:b9 # Proware Technologies Co Ltd. (Rosewill RNX-N150HG USB Adapter)
 )
