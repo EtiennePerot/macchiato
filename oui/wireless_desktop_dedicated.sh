c58da62a348e971eb3a3ce7036db6f8814abdfb8
Etienne Perot <etienne@perot.me>
2013-03-23 17:32:22 -0400

    +1 OUI prefix from http://www.reddit.com/r/privacy/comments/1avca3/got_any_internetconnected_devices_yes_you_do_help/c91461b

--- oui/wireless_desktop_dedicated.sh
+++ oui/wireless_desktop_dedicated.sh
@@ -1,5 +1,6 @@
 # Dedicated (PCI/PCI-E) desktop wireless cards
 oui_wireless_desktop_dedicated=(
 	00:0d:88='D-Link Corporation|D-Link AirPlus XTremeG DWL-G520'
+	10:bf:48='ASUSTeK Computer Inc.|ASUS PCE-N10 Wireless-N PCI Express Adapter'
 	64:70:02='TP-Link Technologies CO., LTD.|Qualcomm Atheros AR9287 Wireless Network Adapter'
 )
