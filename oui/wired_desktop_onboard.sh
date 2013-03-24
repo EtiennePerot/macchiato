1524b2d72171d2a2d08e797853979e890f8c0d6c
Etienne Perot <etienne@perot.me>
2013-03-24 15:11:44 -0400

    +1 OUI from http://www.reddit.com/r/privacy/comments/1avca3/got_any_internetconnected_devices_yes_you_do_help/c91l1ii

--- oui/wired_desktop_onboard.sh
+++ oui/wired_desktop_onboard.sh
@@ -1,6 +1,7 @@
 # Integrated wired network interfaces found in desktop motherboards
 oui_wired_desktop_onboard=(
 	00:17:31='Asustek Computer Inc|A8N-SLI motherboard'
+	00:19:db='MICRO-STAR INTERNATIONAL CO., LTD.|MSI P965 Neo'
 	00:1e:8c='Asustek Computer Inc|CROSSHAIR motherboard'
 	00:25:22='Asrock Incorporation|Asrock Z68 Extreme4'
 	50:e5:49='Giga-byte Technology Co.,ltd.|GIGABYTE GA-Z68X-UD3-B3'
