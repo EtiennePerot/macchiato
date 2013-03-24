069b2cee17238a3357259a4e744c255fc2ec3c81
Etienne Perot <etienne@perot.me>
2013-03-24 15:03:06 -0400

    +1 OUI from http://www.reddit.com/r/privacy/comments/1avca3/got_any_internetconnected_devices_yes_you_do_help/c91j3qn

--- oui/wireless_handheld.sh
+++ oui/wireless_handheld.sh
@@ -1,6 +1,7 @@
 # Wireless handheld video game consoles
 oui_wireless_handheld=(
-	00:1b:ea='Nintendo Co., Ltd.|DS Lite'
-	a4:c0:e1='Nintendo Co., Ltd.|3DS'
-	d8:6b:f7='Nintendo Co., Ltd.|3DS'
+	00:1b:ea='Nintendo Co., Ltd.|Nintendo DS Lite'
+	a4:c0:e1='Nintendo Co., Ltd.|Nintendo 3DS'
+	d4:4b:5e='TAIYO YUDEN CO., LTD.|Sony PS Vita'
+	d8:6b:f7='Nintendo Co., Ltd.|Nintendo 3DS'
 )
