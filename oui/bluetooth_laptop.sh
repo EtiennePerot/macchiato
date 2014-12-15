7c060ce7623e083202860064baddf71e21703cca
Felix Eckhofer <felix@eckhofer.com>
2014-12-15 09:25:43 +0100

    Add Toshiba Z30-A

--- oui/bluetooth_laptop.sh
+++ oui/bluetooth_laptop.sh
@@ -4,5 +4,6 @@ oui_bluetooth_laptop=(
 	40:2c:f4='Universal Global Scientific Industrial Co., Ltd.|Lenovo ThinkPad X121e notebook'
 	70:f3:95='Universal Global Scientific Industrial Co., Ltd|HP 425 notebook'
 	b8:8d:12='Apple, Inc.|MacBook Pro 8.1 (13", 2011)'
+	e8:2a:ea='Intel Corporate|Toshiba Z30-A'
 	e8:39:df='Askey Computer|Fujitsu Lifebook E780'
 )
